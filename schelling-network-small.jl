166da15c6873b812cdeb35d752649ca14fe4a8f6
SageAnastasi <34972347+SageAnastasi@users.noreply.github.com>
2024-02-20 12:05:04 +1300

    made a small version of the network
    to get clarity on what links are being made at each timestep, since it isn't legible on the graph with 320 agents

---
+++ schelling-network-small.jl
@@ -0,0 +1,96 @@
+import Pkg
+
+Pkg.activate(".")
+Pkg.instantiate()
+
+using Agents
+using CairoMakie # choosing a plotting backend
+using SimpleWeightedGraphs 
+using Graphs
+using GraphMakie
+using SparseArrays: findnz
+using Random # for reproducibility
+#using InteractiveDynamics -- no longer needed, abmplot is in Agents
+
+@agent SchellingAgent GridAgent{2} begin
+    seg::Int #the number of neighbours in the same group that the agent needs to be happy
+    mood::Bool # whether the agent is happy in its position. (true = happy)
+    group::Int # The group of the agent, determines mood as it interacts with neighbors
+    
+end
+
+function initialize(; 
+    total_agents = 320, 
+    griddims = (20, 20), 
+    seed = 125
+)
+    space = GridSpaceSingle(griddims, periodic = false)
+    properties = Dict(:social => SimpleWeightedGraph(total_agents))
+    rng = Random.Xoshiro(seed)
+    model = UnremovableABM(
+        SchellingAgent, space;
+        properties, rng, scheduler = Schedulers.Randomly()
+    )
+    # populate the model with agents, adding equal amount of the two types of agents
+    # at random positions in the model
+    for n in 1:total_agents
+        agent = SchellingAgent(n, (1, 1), 3, false, n < total_agents / 2 ? 1 : 2)
+        add_agent_single!(agent, model)
+    end
+
+    # intialise the graph adding an edge to spatial neighbors
+    for agent in model.agents
+        for neighbor in nearby_agents(agent, model)
+                add_edge!(model.social, agent.id, neighbor.id)
+        end
+    end
+
+    return model
+end
+
+model = initialize()
+
+
+
+function agent_step!(agent, model)
+    count_neighbors_same_group = 0
+    # For each neighbor, get group and compare to current agent's group
+    # and increment `count_neighbors_same_group` as appropriately.
+    # Here `nearby_agents` (with default arguments) will provide an iterator
+    # over the nearby agents one grid point away, which are at most 8.
+    for neighbor in nearby_agents(agent, model)
+        if agent.group == neighbor.group
+            count_neighbors_same_group += 1
+        end
+    end
+    # After counting the neighbors, decide whether or not to move the agent.
+    # If count_neighbors_same_group is at least the min_to_be_happy, set the
+    # mood to true. Otherwise, move the agent to a random position, and set
+    # mood to false.
+    if count_neighbors_same_group ≥ agent.seg
+        agent.mood = true
+    else
+        agent.mood = false
+        move_agent_single!(agent, model)
+    end
+    return
+end
+
+function model_step!(model)
+    for agent in allagents(model)
+        #check whether the agent has a graph edge with its neighbours, and if not add an edge.
+        for neighbor in nearby_agents(agent, model)
+            if has_edge(model.social, neighbor.id, agent.id) == false
+                add_edge!(model.social, neighbor.id, agent.id)
+            end
+        end
+    end
+end
+
+groupcolor(a) = a.group == 1 ? :blue : :orange
+groupmarker(a) = a.group == 1 ? :circle : :rect
+figure, _ = abmplot(model; ac = groupcolor, am = groupmarker, as = 10)
+figure # returning the figure displays it
+
+graphplot(model.social) 
+
