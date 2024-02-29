a4c1b5fc78d60d548f44e987279ebada18b8f190
SageAnastasi <34972347+SageAnastasi@users.noreply.github.com>
2024-03-01 10:54:34 +1300

    trying to get neighbours in the graph layer; not working

--- schelling-network.jl
+++ schelling-network.jl
@@ -34,7 +34,7 @@ function initialize(;
     # populate the model with agents, adding equal amount of the two types of agents
     # at random positions in the model
     for n in 1:total_agents
-        agent = SchellingAgent(n, (1, 1), 0.375, false, n < total_agents / 2 ? 1 : 2)
+        agent = SchellingAgent(n, (1, 1), 3, false, n < total_agents / 2 ? 1 : 2)
         add_agent_single!(agent, model)
     end
 
@@ -58,16 +58,18 @@ function agent_step!(agent, model)
     # and increment `count_neighbors_same_group` as appropriately.
     # Here `nearby_agents` (with default arguments) will provide an iterator
     # over the nearby agents one grid point away, which are at most 8.
-    for neighbor in neighbours(agent,1)
-        if agent.group == neighbor.group
+    neigh = Graphs.neighbors(model.social, 1)
+    for i in neigh
+        if agent.group == model[i].group
             count_neighbors_same_group += 1
         end
+        print(count_neighbors_same_group)
     end
     # After counting the neighbors, decide whether or not to move the agent.
     # If count_neighbors_same_group is at least the min_to_be_happy, set the
     # mood to true. Otherwise, move the agent to a random position, and set
     # mood to false.
-    if (count_neighbors_same_group)/neighbours(agent,1) ≥ agent.seg
+    if count_neighbors_same_group ≥ agent.seg
         agent.mood = true
     else
         agent.mood = false
