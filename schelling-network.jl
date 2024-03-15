c4900958a95a80462cef2490e2095e7ff661512a
SageAnastasi <34972347+SageAnastasi@users.noreply.github.com>
2024-03-15 14:35:23 +1300

    changed neighbour counting to use agent ID number rather than placeholder

--- schelling-network.jl
+++ schelling-network.jl
@@ -38,13 +38,6 @@ function initialize(;
         add_agent_single!(agent, model)
     end
 
-    # intialise the graph adding an edge to spatial neighbors
-    #for agent in model.agents
-        #for neighbor in nearby_agents(agent, model)
-                #add_edge!(model.social, agent.id, neighbor.id)
-        #end
-    #end
-
     for agent in model.agents
         for n in 1:8
             friend = rand(1:320)    
@@ -62,15 +55,16 @@ model = initialize()
 function agent_step!(agent, model)
     count_neighbors_same_group = 0
     count_neighbours = 0
+    which_agent = agent.id
 
     # For each neighbor, get group and compare to current agent's group
     # and increment `count_neighbors_same_group` as appropriately.
     # Here `nearby_agents` (with default arguments) will provide an iterator
     # over the nearby agents one grid point away, which are at most 8.
-    neigh = Graphs.neighbors(model.social, 1)
+    neigh = Graphs.neighbors(model.social, which_agent)
     for i in neigh
         count_neighbours += 1
-        if model[1].group == model[i].group
+        if model[which_agent].group == model[i].group
             count_neighbors_same_group += 1
         end
         #print(count_neighbors_same_group)
