cdf6caf8c768c875bd45c4815aa88b814a794559
SageAnastasi <34972347+SageAnastasi@users.noreply.github.com>
2024-03-15 13:55:34 +1300

    changed seg type from int to float64

--- schelling-network.jl
+++ schelling-network.jl
@@ -13,7 +13,7 @@ using Random # for reproducibility
 #using InteractiveDynamics -- no longer needed, abmplot is in Agents
 
 @agent SchellingAgent GridAgent{2} begin
-    seg::Int #the number of neighbours in the same group that the agent needs to be happy
+    seg::Float64 #the number of neighbours in the same group that the agent needs to be happy
     mood::Bool # whether the agent is happy in its position. (true = happy)
     group::Int # The group of the agent, determines mood as it interacts with neighbors
     
@@ -45,12 +45,12 @@ function initialize(;
         end
     end
 
-    for agent in model.agents
-        for n in 1:8
-            friend = rand(1:320)    
-            add_edge!(model.social, agent.id, friend)
-        end
-    end
+    #for agent in model.agents
+        #for n in 1:8
+            #friend = rand(1:320)    
+            #add_edge!(model.social, agent.id, friend)
+        #end
+    #end
 
     return model
 end
@@ -62,15 +62,15 @@ model = initialize()
 function agent_step!(agent, model)
     count_neighbors_same_group = 0
     count_neighbours = 0
-    which_agent = agent.id
+
     # For each neighbor, get group and compare to current agent's group
     # and increment `count_neighbors_same_group` as appropriately.
     # Here `nearby_agents` (with default arguments) will provide an iterator
     # over the nearby agents one grid point away, which are at most 8.
-    neigh = Graphs.neighbors(model.social, which_agent)
+    neigh = Graphs.neighbors(model.social, 1)
     for i in neigh
         count_neighbours += 1
-        if model[which_agent].group == model[i].group
+        if model[1].group == model[i].group
             count_neighbors_same_group += 1
         end
         #print(count_neighbors_same_group)
