60fd29d520b521bff2b57907351cea24538da9c0
SageAnastasi <34972347+SageAnastasi@users.noreply.github.com>
2024-04-05 13:06:59 +1300

    updated the overall model with improvements from eight-friends

--- schelling-network.jl
+++ schelling-network.jl
@@ -12,97 +12,102 @@ using SparseArrays: findnz
 using Random # for reproducibility
 #using InteractiveDynamics -- no longer needed, abmplot is in Agents
 
-@agent SchellingAgent GridAgent{2} begin
-    seg::Float64 #the number of neighbours in the same group that the agent needs to be happy
-    mood::Bool # whether the agent is happy in its position. (true = happy)
-    group::Int # The group of the agent, determines mood as it interacts with neighbors
-    
-end
-
-function initialize(; 
-    total_agents = 320, 
-    griddims = (20, 20), 
-    seed = 125
-)
-    space = GridSpaceSingle(griddims, periodic = false)
-    properties = Dict(:social => SimpleWeightedGraph(total_agents))
-    rng = Random.Xoshiro(seed)
-    model = UnremovableABM(
-        SchellingAgent, space;
-        properties, rng, scheduler = Schedulers.Randomly()
-    )
-    # populate the model with agents, adding equal amount of the two types of agents
-    # at random positions in the model
-    for n in 1:total_agents
-        agent = SchellingAgent(n, (1, 1), 0.3, false, n < total_agents / 2 ? 1 : 2)
-        add_agent_single!(agent, model)
+function randomExcluded(min, max, excluded)
+      
+    n = rand(min:max)
+    if (n ≥ excluded) 
+        n += 1
+    else
+        n += 0
     end
 
-    # intialise the graph adding an edge to spatial neighbors
-    #for agent in model.agents
-        #for neighbor in nearby_agents(agent, model)
-                #add_edge!(model.social, agent.id, neighbor.id)
-        #end
-    #end
+end #function needed for generating random graph edges without node selecting itself
 
-    for agent in model.agents
-        for n in 1:8
-            friend = rand(1:320)    
-            add_edge!(model.social, agent.id, friend)
+@agent struct SchellingAgent(GridAgent{2}) begin
+    mood::Bool # whether the agent is happy in its position. (true = happy)
+    group::Int # The group of the agent, determines mood as it interacts with neighbors
+    seg::Float64 #the number of neighbours in the same group that the agent needs to be happy
+    
+end
+end
+
+function schelling_step!(agent, model)
+    count_neighbours_same_group = 0
+    count_neighbours = 0
+    which_agent = agent.id
+    
+    neigh = Graphs.neighbors(model.social, which_agent)
+    neighbours_same_group = []
+    neighbours_other_group = []
+    for i in neigh
+        count_neighbours += 1
+        if model[which_agent].group == model[i].group
+            count_neighbours_same_group += 1
+            push!(neighbours_same_group,model[i].id)
+        else 
+            push!(neighbours_other_group,model[i].id)
         end
+    end #keeping track of the agent's same and different group links to select from later
+
+    if count_neighbours_same_group/count_neighbours ≥ agent.seg
+        agent.mood = true
+    else
+        agent.mood = false
+        cutoff = rand(neighbours_other_group)
+        rem_edge!(model.social, which_agent, cutoff)
+        count_neighbours -=1
+    end #if unhappy, cut off a link from a different group
+
+    while count_neighbours ≤ 8 #each node should have at least 4 friends, this can be disrupted by incoming links being broken
+        if length(neighbours_same_group) > 0
+            network_link = rand(neighbours_same_group)
+            friends_of_friend = Graphs.neighbors(model.social, network_link) 
+            friends_of_friend = setdiff(friends_of_friend,which_agent)
+            new_friend = rand(friends_of_friend) 
+            add_edge!(model.social,which_agent,new_friend)
+            count_neighbours +=1    #if there are friends in the same group, select new freind from their friends at random
+        else
+            random_friend = randomExcluded(1,99999,which_agent)
+            add_edge!(model.social,which_agent,random_friend)
+            count_neighbours +=1    #else select a friend from the whole graph at random
+        end
+    end
+    
+    return
+end
+
+function initialize(; total_agents = 100000, gridsize = (20, 20), seed = 125)
+    space = GridSpaceSingle(gridsize; periodic = false)
+    properties = Dict(:social => SimpleWeightedGraph(total_agents))
+    rng = Xoshiro(seed)
+    model = StandardABM(
+        SchellingAgent, space;
+        agent_step! = schelling_step!, properties, rng,
+        container = Vector, # agents are not removed, so we us this
+        scheduler = Schedulers.Randomly() # all agents are activated once at random
+    )
+    # populate the model with agents, adding equal amount of the two types of agents
+    # at random positions in the model. At the start all agents are unhappy.
+    for n in 1:total_agents
+        add_agent_single!(model; mood = false, group = n < total_agents / 2 ? 1 : 2, seg = 0.375)
     end
 
     return model
+
+   
+
+    
 end
 
 model = initialize()
 
-
-
-function agent_step!(agent, model)
-    count_neighbors_same_group = 0
-    count_neighbours = 0
-
-    # For each neighbor, get group and compare to current agent's group
-    # and increment `count_neighbors_same_group` as appropriately.
-    # Here `nearby_agents` (with default arguments) will provide an iterator
-    # over the nearby agents one grid point away, which are at most 8.
-    neigh = Graphs.neighbors(model.social, 1)
-    for i in neigh
-        count_neighbours += 1
-        if model[1].group == model[i].group
-            count_neighbors_same_group += 1
-        end
-        #print(count_neighbors_same_group)
-    end
-    # After counting the neighbors, decide whether or not to move the agent.
-    # If count_neighbors_same_group is at least the min_to_be_happy, set the
-    # mood to true. Otherwise, move the agent to a random position, and set
-    # mood to false.
-    if count_neighbors_same_group/count_neighbours ≥ agent.seg
-        agent.mood = true
-    else
-        agent.mood = false
-        move_agent_single!(agent, model)
-    end
-    return
-end
-
-function model_step!(model)
-    for agent in allagents(model)
-        #check whether the agent has a graph edge with its neighbours, and if not add an edge.
-        for neighbor in nearby_agents(agent, model)
-            if has_edge(model.social, neighbor.id, agent.id) == false
-                add_edge!(model.social, neighbor.id, agent.id)
-            end
-        end
+for n in 1:99999 #populate the model with graph edges
+    starter_agent = n
+    for n in 1:149
+        friend = randomExcluded(1,319,starter_agent)
+        add_edge!(model.social, starter_agent, friend)
     end
 end
 
-groupcolor(a) = a.group == 1 ? :blue : :orange
-groupmarker(a) = a.group == 1 ? :circle : :rect
-figure, _ = abmplot(model; ac = groupcolor, am = groupmarker, as = 10)
-figure # returning the figure displays it
-
-graphplot(model.social) 
+graphplot(model.social)
 
